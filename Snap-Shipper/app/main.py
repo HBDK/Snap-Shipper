b06f70df825f78198883fdbe5e89e1690009df76
hbdk <haraldbilsoe@gmail.com>
2020-10-04 15:26:22 +0200

    remove entry

--- Snap-Shipper/app/main.py
+++ Snap-Shipper/app/main.py
@@ -4,23 +4,21 @@ from Options import OptionsBuilder
 from SinkFactory import SinkFactory
 import logging
 
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
+logging.info("Starting Snap-Shipper")
 
-def entrypoint():
-    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
-    logging.info("Starting Snap-Shipper")
+options = OptionsBuilder().Options
 
-    options = OptionsBuilder().Options
+clients =  SinkFactory(options.Sinks).Sinks
 
-    clients =  SinkFactory(options.Sinks).Sinks
-
-    for file in listdir(options.Folder):
-        path = join(options.Folder,file)
-        logging.info("Processing: {}".format(path))
-        if isfile(path):
-            for client in clients:
-                if not file in client.Remotes:
-                    logging.info("uploading: {} To: {}".format(file,client.Name))
-                    if not options.DryRun:
-                        client.Upload(remote_path=file, local_path=path)
-                else:
-                    logging.info("Skipping: {} for: {}".format(file,client.Name))
+for file in listdir(options.Folder):
+    path = join(options.Folder,file)
+    logging.info("Processing: {}".format(path))
+    if isfile(path):
+        for client in clients:
+            if not file in client.Remotes:
+                logging.info("uploading: {} To: {}".format(file,client.Name))
+                if not options.DryRun:
+                    client.Upload(remote_path=file, local_path=path)
+            else:
+                logging.info("Skipping: {} for: {}".format(file,client.Name))
